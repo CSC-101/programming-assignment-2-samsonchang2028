d9f7e39336202e875d04ec25bf46097de481e054
sfkim <samson.chang2@gmail.com>
2024-10-31 00:00:28 -0700

    firdt commit

--- hw2.py
+++ hw2.py
@@ -3,18 +3,82 @@ import data
 # Write your functions for each part in the space below.
 
 # Part 1
-
+def create_rectangle(point1:data.Point,point2:data.Point)->data.Rectangle:
+    x1=min(point1.x,point2.x)
+    y1= max(point1.y,point2.y)
+    topleft = data.Point(x1,y1) #the top left of a rectangle needs the smallest x value but the biggest y value
+    x2 = max(point1.x,point2.x)
+    y2= min(point1.y,point2.y)
+    bottomright = data.Point(x2,y2)# the bottom right of a rectangle needs the smallest y value and the biggest x value
+    return data.Rectangle(topleft,bottomright)
 
 # Part 2
-
+def shorter_duration_than(time1:data.Duration,time2:data.Duration)->bool:
+    if time1.minutes < time2.minutes: # if the minutes value is bigger, then the time duration will be bigger, this checks if
+        #time1 is less than time2
+        return True
+    elif time1.minutes == time2.minutes:# if the minutes are the same, this compares time1's seconds and time2's to see
+        #which is bigger
+        if time1.seconds < time2.seconds:
+            return True
+    else:
+        return False
 
 # Part 3
+def songs_shorter_than(song_list:list[data.Song],song_length:data.Duration)->list[data.Song]:
+   new_song_list = [] #creates a new list for the songs that will be less than said time
+   for song in song_list:
+        if song.duration.minutes < song_length.minutes: # checks the same as before in part 2,  if the minutes value is bigger, then
+        # the time duration will be bigger, this checks if
+        #the songs in the list are less than the upperbound duration
+            new_song_list.append(song) # adds teh song to the new list if it's smaller than said duration
+        elif song.duration.minutes == song_length.minutes: # checks the seconds to see if the song is within the upper bound
+            if song.duration.seconds < song_length.seconds:
+                new_song_list.append(song)
+   return new_song_list
+    # return [songs for songs in song_list if songs.duration < song_length]
 
 
 # Part 4
+def running_time(song_list:list[data.Song],number:list[int])->data.Duration:
+    minutes = 0
+    seconds =0
+    duration = data.Duration(minutes, seconds)
+    for i in number:
+        duration.minutes = duration.minutes + song_list[number[i]].duration.minutes
+        duration.seconds = duration.seconds + song_list[number[i]].duration.seconds #adds the seconds and minutes together from the list
+        if duration.seconds > 60: # if the secodns value is bigger than 60, adds 1 to minute and resets back value- 60
+            duration.minutes = duration.minutes + 1
+            duration.seconds = duration.seconds - 60
 
-
+            print(duration)
+    return duration
 # Part 5
+def validate_route(city_links:list[list[str]],city_names_route:list[str])->bool:
+    for i in range(len(city_names_route) - 1):
+        city1 = city_names_route[i] #first in the citynamesroute list
+        city2 = city_names_route[i+1] #second in the citynamesroute list
+        if [city1,city2] not in city_links and [city2,city1] not in city_links:# checks if they are linked backwards or forwards
+            return False
+    return True
 
 
 # Part 6
+def longest_repetition(values:list[int]):
+    if len(values) < 1:
+        return None
+    long_repetition = 1
+    long_index = 0
+    current_len =1
+    current_idx =0
+    for i in range(len(values)-1):
+        if values[i] == values[i-1]
+            current_len +=1
+        elif current_len > long_repetition:
+            long_repetition = current_len
+            long_index = current_idx
+        current_len = i
+        current_idx = i
+    if current_len > long_repetition:
+        long_index = current_idx
+    return long_index
--- hw2_tests.py
+++ hw2_tests.py
@@ -7,23 +7,93 @@ import unittest
 
 class TestCases(unittest.TestCase):
     # Part 1
-
-
+    def test_create_rectangle_1(self):
+        point1 = data.Point(2,2)
+        point2 = data.Point(10,10)
+        results = hw2.create_rectangle(point1,point2)
+        expected = data.Rectangle(data.Point(2,10),data.Point(10,2))
+        self.assertEqual(expected , results)
+    def test_create_rectangle_2(self):
+        point1 = data.Point(2,2)
+        point2 = data.Point(2,2)
+        results = hw2.create_rectangle(point1,point2)
+        expected = data.Rectangle(data.Point(2,2),data.Point(2,2))
+        self.assertEqual(expected , results)
     # Part 2
-
-
+    def test_shorter_duration_than_1(self):
+        time1 = data.Duration(4,19)
+        time2 = data.Duration(4,59)
+        results = hw2.shorter_duration_than(time1,time2)
+        expected = True
+        self.assertEqual(expected,results)
+    def test_shorter_duration_than_2(self):
+        time1 = data.Duration(18,19)
+        time2 = data.Duration(4,59)
+        results = hw2.shorter_duration_than(time1,time2)
+        expected = False
+        self.assertEqual(expected,results)
     # Part 3
+    def test_songs_shorter_than_1(self):
+        song_list = [data.Song('Wallen','More Than My Hometown',data.Duration(3,37)),data.Song('1D','Night Changes',data.Duration(4,29))]
+        song_length = data.Duration(4,30)
+        results = hw2.songs_shorter_than(song_list,song_length)
+        expected = [data.Song('Wallen','More Than My Hometown',data.Duration(3,37)),data.Song('1D','Night Changes',data.Duration(4,29))]
+        self.assertEqual(expected,results)
 
-
+    def test_songs_shorter_than_2(self):
+        song_list = [data.Song('Drake','Hours in Silence',data.Duration(6,39)),data.Song('Jhene','Sativa',data.Duration(4,37))]
+        song_length = data.Duration(4,30)
+        results = hw2.songs_shorter_than(song_list,song_length)
+        expected = []
+        self.assertEqual(expected,results)
     # Part 4
-
-
+    def test_running_time_1(self):
+        song_list = [data.Song('Drake','Hours in Silence',data.Duration(6,39)),data.Song('Jhene','Sativa',data.Duration(4,37)),data.Song('Wallen','More Than My Hometown',data.Duration(3,37)),data.Song('1D','Night Changes',data.Duration(4,29))]
+        number = [0,1,2,3]
+        results = hw2.running_time(song_list,number)
+        expected = data.Duration(19,22)
+        self.assertEqual(expected,results)
+    def test_running_time_2(self):
+        song_list = [data.Song('Jackson 5','I Want You Back',data.Duration(3,29)),data.Song('Natasha','Unwritten',data.Duration(4,19)),data.Song('Wallen','7 Summers',data.Duration(3,37)),data.Song('Billy Joel','Uptown Girl',data.Duration(3,18))]
+        number = [0,1,2,3]
+        results = hw2.running_time(song_list,number)
+        expected = data.Duration(19,22)
+        self.assertEqual(expected,results)
     # Part 5
+    def test_validate_route(self):
+        city_links = [
+            ['san luis obispo', 'santa margarita'],
+            ['san luis obispo', 'pismo beach'],
+            ['atascadero', 'santa margarita'],
+            ['atascadero', 'creston']
+        ]
+        city_route = ['san luis obispo', 'santa margarita', 'atascadero']
+        results = hw2.validate_route(city_links,city_route)
+        expected = True
+        self.assertEqual(expected,results)
 
-
+    def test_validate_route_2(self):
+        city_links = [
+            ['san luis obispo', 'santa margarita'],
+            ['san luis obispo', 'pismo beach'],
+            ['atascadero', 'santa margarita'],
+            ['atascadero', 'creston']
+        ]
+        city_route = ['san luis obispo', 'creston', 'atascadero']
+        results = hw2.validate_route(city_links, city_route)
+        expected = False
+        self.assertEqual(expected, results)
     # Part 6
-
-
+    def test_longest_rep_1(self):
+        values = [1,1,1,2,3,4]
+        results = hw2.longest_repetition(values)
+        expected = 3
+        self.assertEqual(expected,results)
+    def test_longest_rep_2(self):
+        values = [1,1,2,2,3,4,5,5,5,5]
+        results = hw2.longest_repetition(values)
+        expected = 4
+        self.assertEqual(expected,results)
 
 
 
